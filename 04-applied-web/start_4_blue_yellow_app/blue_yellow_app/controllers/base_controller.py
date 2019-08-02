fb2b35d191494fa1d8a55cc5cbd143da425f4898
Jordan Petroff <jordan@jeponline.net>
2019-08-02 10:43:13 -0500

    Added account controller and get/post for registration form

--- 04-applied-web/start_4_blue_yellow_app/blue_yellow_app/controllers/base_controller.py
+++ 04-applied-web/start_4_blue_yellow_app/blue_yellow_app/controllers/base_controller.py
@@ -1,7 +1,22 @@
 from blue_yellow_app.infrastructure import static_cache
+import pyramid.renderers
+import pyramid.httpexceptions as exc
 
 
 class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = static_cache.build_cache_id
+
+        layout_render = pyramid.renderers.get_renderer('blue_yellow_app:templates/shared/_layout.pt')
+        impl = layout_render.implementation()
+        self.layout = impl.macros['layout']
+
+    @property
+    def is_logged_in(self):
+        return False
+
+    def redirect(self, to_url, permanent=False):
+        if permanent:
+            raise exc.HTTPMovedPermanently(to_url)
+        raise exc.HTTPFound(to_url)
